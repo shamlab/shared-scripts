77819fab78c0d0e7e3a48fdb802d29d175390e21
jacobhsu <jacobhsu@hku.hk>
2017-03-24 14:37:08 +0800

    Use variable PBS_O_WORKDIR, so everyone can use it directly

--- NGS/QC/Kggseq_QC/qsub.sh
+++ NGS/QC/Kggseq_QC/qsub.sh
@@ -6,9 +6,11 @@
 #PBS -q medium
 #PBS -N KGGseq_QC
 
-Path=/home/jacobhsu/scripts/shared-scripts
-txt=$Path/NGS/QC/Kggseq_QC/Kggseq_basicQC.txt
+
+cd $PBS_O_WORKDIR &&
+
+Path=$(pwd)
+txt=$Path/Kggseq_basicQC.txt
 kggseq=/home/groups/pcsham/shared/kggseq_V1/20160816/kggseq20160924.jar
 
-cd $Path/NGS/QC/Kggseq_QC &&
 java -jar $kggseq $txt
